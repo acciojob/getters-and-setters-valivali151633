5d56048fe2b000b69ac8f92d5fb7f31cbc733988
valivali151633 <144232966+valivali151633@users.noreply.github.com>
2024-04-01 21:05:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,16 +2,21 @@
 class Person {
 	constructor(name, age)
 	{
-		this.name = name;
-		this.age = age;
+		this._name = name;
+		this._age = age;
 	}
- get getterName() {
-	 return this.name;
- }
+ 
+	get name() {
+		return this._name;
+	}
 
- set setage(age) {
-	 this.age = age;
- }
+	set age(age) {
+		this._age = age;
+	}
+
+	get age() {
+		return this._age;
+	}
 }
 
 class Student extends Person {
@@ -31,6 +36,20 @@ class Teacher extends Person {
 		console.log(`${this.name} is teaching`)
 	}
 }
+
+// const person = new Person("John", 25);
+// console.log(person.name); // Output: John
+// console.log(person.age); // Output: 25
+
+// person.age = 30;
+// console.log(person.age); // Output: 30
+
+// const student = new Student("Alice", 22);
+// student.study(); // Output: Alice is studying
+
+// const teacher = new Teacher("Bob", 40);
+// teacher.teach(); // Output: Bob is teaching
+
 
 // Do not change the code below this line
 window.Person = Person;
